a5e0c504760f5bd78fdb7e4313a94c55b4455323
Adam Kennedy <adam@ali.as>
2007-07-01 05:21:17 +0000

    Correcting email address

--- lib/Perl/MinimumVersion.pm
+++ lib/Perl/MinimumVersion.pm
@@ -545,7 +545,7 @@ For other issues, or commercial enhancement or support, contact the author.
 
 =head1 AUTHORS
 
-Adam Kennedy E<lt>cpan@ali.asE<gt>
+Adam Kennedy E<lt>adamk@cpan.orgE<gt>
 
 =head1 SEE ALSO
 
