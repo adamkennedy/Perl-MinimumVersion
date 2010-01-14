34f0977beccb6b428be2a10602404746cad1db50
Adam Kennedy <adam@ali.as>
2010-01-14 05:39:23 +0000

    Updating copyright

--- lib/Perl/MinimumVersion.pm
+++ lib/Perl/MinimumVersion.pm
@@ -972,7 +972,7 @@ L<http://ali.as/>, L<PPI>, L<version>
 
 =head1 COPYRIGHT
 
-Copyright 2005 - 2009 Adam Kennedy.
+Copyright 2005 - 2010 Adam Kennedy.
 
 This program is free software; you can redistribute
 it and/or modify it under the same terms as Perl itself.
