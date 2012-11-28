b51a151b92b94496adabdac8e979ba7e7b14af62
Alexandr Ciornii <alexchorny@gmail.com>
2012-11-28 21:30:34 +0000

    version 1.30

--- lib/Perl/MinimumVersion.pm
+++ lib/Perl/MinimumVersion.pm
@@ -53,7 +53,7 @@ use Perl::MinimumVersion::Reason ();
 
 our ($VERSION, @ISA, @EXPORT_OK, %CHECKS, @CHECKS_RV ,%MATCHES);
 BEGIN {
-	$VERSION = '1.29';
+	$VERSION = '1.30';
 
 	# Only needed for dev releases, comment out otherwise
 	# $VERSION = eval $VERSION;
--- lib/Perl/MinimumVersion/Reason.pm
+++ lib/Perl/MinimumVersion/Reason.pm
@@ -10,7 +10,7 @@ use warnings;
 
 use vars qw{$VERSION};
 BEGIN {
-	$VERSION = '1.29';
+	$VERSION = '1.20';
 
 	# Only needed for dev releases, comment out otherwise
 	# $VERSION = eval $VERSION;
