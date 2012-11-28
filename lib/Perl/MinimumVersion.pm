7d146d48962e147b6bf748a079a483628b057ce2
Alexandr Ciornii <alexchorny@gmail.com>
2012-11-28 15:19:49 +0000

    'each % { $foo }' incorrectly required perl 5.14

--- lib/Perl/MinimumVersion.pm
+++ lib/Perl/MinimumVersion.pm
@@ -630,6 +630,8 @@ sub _each_argument {
 				$version = 5.014;
 				$obj = $_[1]->parent;
 			}
+		} elsif($next->isa('PPI::Token::Operator')) { # % $a
+			return '';
 		} else { # function call or other should be reference
 			if(5.014 > ($version || 0)) {
 				$version = 5.014;
